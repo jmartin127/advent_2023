9dc4d42d5daaeb4274d0afe9c58ebce373559cf1
Jeff Martin <jmartin127@gmail.com>
2023-12-02 20:15:59 -0700

    Clean up day 2, part 1

--- lib/day2/part1/run.dart
+++ lib/day2/part1/run.dart
@@ -6,17 +6,16 @@ void main(List<String> arguments) async {
   final lines = await readInput();
 
   int result = 0;
+
+  // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
   await for (var line in lines) {
-    print('$line');
-    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     final gamePrefix = line.split(': ');
     final gameNum = int.parse(gamePrefix[0].split(' ')[1]);
-    print(gameNum);
-    final subGames = gamePrefix[1].split('; ');
     bool allPossible = true;
-    for (final sub in subGames) {
+    for (final sub in gamePrefix[1].split('; ')) {
       if (!isPossible(sub)) {
         allPossible = false;
+        break;
       }
     }
     if (allPossible) {
@@ -31,7 +30,6 @@ bool isPossible(String sub) {
     final colorParts = colorGroup.split(' ');
     final count = int.parse(colorParts[0]);
     final color = colorParts[1];
-    print('Count: $count, Bag: ${colorCounts[color]}');
     if (count > colorCounts[color]!) {
       return false;
     }
